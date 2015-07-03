7e4a18b10b95f07a6f84190a8360a66bce769f2b
Pietro.Marini@keyrus.com <Pietro.Marini@keyrus.com>
2015-07-03 14:23:09 +0200

    Model 2

--- feature_extractor.py
+++ feature_extractor.py
@@ -1,10 +1,5 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Jul  3 11:58:43 2015
-
-@author: pietro
-"""
 import pandas as pd
+
 class FeatureExtractor(object):
     def __init__(self):
         pass
@@ -12,24 +7,30 @@ class FeatureExtractor(object):
     def fit(self, X_df, y_array):
         pass
 
-    def transform(self, X_df):
-        X_encoded = X_df
-        
-        #uncomment the line below in the submission
-        #path = os.path.dirname(__file__)
-        data_weather = pd.read_csv("data_weather.csv")
-        
-        X_weather = data_weather[['Date', 'AirPort', 'Max TemperatureC']]
-        X_weather = X_weather.rename(columns={'Date': 'DateOfDeparture', 'AirPort': 'Arrival'})
-        X_encoded = X_encoded.set_index(['DateOfDeparture', 'Arrival'])
-        X_weather = X_weather.set_index(['DateOfDeparture', 'Arrival'])
-        X_encoded = X_encoded.join(X_weather).reset_index()
-        
-        X_encoded = X_encoded.join(pd.get_dummies(X_encoded['Departure'], prefix='d'))
-        X_encoded = X_encoded.join(pd.get_dummies(X_encoded['Arrival'], prefix='a'))
-        X_encoded = X_encoded.drop('Departure', axis=1)
-        X_encoded = X_encoded.drop('Arrival', axis=1)
+    def transform(self, data_encoded):
 
-        X_encoded = X_encoded.drop('DateOfDeparture', axis=1)
-        X_array = X_encoded.values
+        data_encoded = data_encoded.join(pd.get_dummies(data_encoded['Departure'], prefix='d'))
+        data_encoded = data_encoded.join(pd.get_dummies(data_encoded['Arrival'], prefix='a'))
+        data_encoded = data_encoded.drop('Departure', axis=1)
+        data_encoded = data_encoded.drop('Arrival', axis=1)
+
+        
+        data_encoded['DateOfDeparture'] = pd.to_datetime(data_encoded['DateOfDeparture'])
+        data_encoded['year'] = data_encoded['DateOfDeparture'].dt.year
+        data_encoded['month'] = data_encoded['DateOfDeparture'].dt.month
+        data_encoded['day'] = data_encoded['DateOfDeparture'].dt.day
+        data_encoded['weekday'] = data_encoded['DateOfDeparture'].dt.weekday
+        data_encoded['week'] = data_encoded['DateOfDeparture'].dt.week
+        data_encoded['n_days'] = data_encoded['DateOfDeparture'].apply(lambda date: (date - pd.to_datetime("1970-01-01")).days)
+
+        data_encoded = data_encoded.join(pd.get_dummies(data_encoded['year'], prefix='y'))
+        data_encoded = data_encoded.join(pd.get_dummies(data_encoded['month'], prefix='m'))
+        data_encoded = data_encoded.join(pd.get_dummies(data_encoded['day'], prefix='d'))
+        data_encoded = data_encoded.join(pd.get_dummies(data_encoded['weekday'], prefix='wd'))
+        data_encoded = data_encoded.join(pd.get_dummies(data_encoded['week'], prefix='w'))
+        
+        data_encoded = data_encoded.drop(['DateOfDeparture'], axis=1)
+        
+        X_array = np.array(data_encoded)
+        
         return X_array
--- regressor.py
+++ regressor.py
@@ -1,16 +1,9 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Jul  3 11:57:55 2015
-
-@author: pietro
-"""
-
 from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.base import BaseEstimator
 
 class Regressor(BaseEstimator):
     def __init__(self):
-        self.clf = GradientBoostingRegressor( n_estimators = 1500 , max_features = 20 )
+        self.clf = GradientBoostingRegressor( n_estimators = 10 , max_features = 20 )
 
     def fit(self, X, y):
         self.clf.fit(X, y)
