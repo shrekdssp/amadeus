7738247ede5f0fb255bd6f1aa8e922f96a4eda9b
Pietro.Marini@keyrus.com <Pietro.Marini@keyrus.com>
2015-07-04 12:03:33 +0200

    Commit 3 - 04/07

--- regressor.py
+++ regressor.py
@@ -1,14 +1,12 @@
-from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.base import BaseEstimator
-
-class Regressor(BaseEstimator ):
+import xgboost as xgb
+ 
+class Regressor(BaseEstimator):
     def __init__(self):
-        self.clf = GradientBoostingRegressor( n_estimators = 1500 , max_depth = 6 , max_features = 20 , verbose = 1)
-        print("N. estimators:\t%i" % (self.clf.n_estimators) )
-        print("Max features:\t%i" %  (self.clf.max_features) )
-        print("Max features:\t%i" % (self.clf.max_depth) ) 
+        self.clf = xgb.XGBRegressor(max_depth=17, n_estimators=1000, learning_rate=0.05)
+ 
     def fit(self, X, y):
         self.clf.fit(X, y)
-
+ 
     def predict(self, X):
         return self.clf.predict(X)
